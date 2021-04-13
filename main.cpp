d853c46d63ef5d30af6e001f23a3b9e7c919580b
tdolega <tymoteuszdolega@gmail.com>
2021-04-13 20:36:18 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <execution>
+#include <algorithm>
+#include <parallel/algorithm>
 #include <thread>
+//#include <mutex>
 
 using std::cout;
 using std::endl;
@@ -13,22 +17,18 @@ using std::endl;
 //const int W = 1200;
 //const int H = 700;
 //int brushSize = 20;
+//const int PIXEL_SIZE = 1;
 
 const int W = 256;
 const int H = 144;
 int brushSize = 3;
-
 const int PIXEL_SIZE = 4;
 
+
 //const float LIFETIME_MS = 50000;
+const int FILL_PERC = 1;
 
-const int ITHREADS = 16;
-
-const unsigned SAND_C = 0xc2b280ff;
-const unsigned WATER_C = 0x2389daff;
-const unsigned STONE_C = 0x888c8dff;
-const unsigned PINK_C = 0xe324d6ff;
-const unsigned EMPTY_C = 0;
+const int ITHREADS = 4;
 
 uint64_t wyhash64_x;
 uint64_t myrand() {
@@ -45,35 +45,58 @@ uint64_t myrand() {
 
 //const sf::Time LIFETIME = sf::seconds(LIFETIME_MS);
 
-const unsigned SAND   = 0b000001;
-const unsigned WATER  = 0b000010;
-const unsigned STONE  = 0b000100;
-const unsigned EMPTY  = 0;
+enum types { // color and id
+    T_EMPTY     = 0,
+    T_SAND      = 0xc2b280ff,
+    T_WATER     = 0x2389daff,
+    T_STONE     = 0x888c8dff,
+    T_LAVA,
+    T_FIRE,
+    T_SMOKE,
+    T_SANDPINK  = 0xe324d6ff,
+};
 
-class Particle {
-public:
-    int type = 0;
+enum moves {
+    MOVES_NONE  = 0b000000,
+    MOVES_U     = 0b000001,
+    MOVES_LR    = 0b000010,
+    MOVES_D     = 0b000100,
+    MOVES_LRD   = 0b001000,
+    MOVES_LRU   = 0b010000,
+    MOVES_FORCE = 0b100000,
+};
+
+struct Particle {
+    types type = T_EMPTY;
     sf::Time lifetime;
-    bool updated = false;
-    sf::Vector2f velocity {0, 0};
-
-    Particle() {
-//        float angle = ((myrand() % 20) + 80) * 3.14f / 180.f;
-//        float speed = (myrand() % 50) + 50.f;
-//        velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-    }
+    sf::Vector2i velocity {0, 0};
+    int hp = 100;
 
     void setLifetime(sf::Time lifetimeAprox) {
         auto lax = lifetimeAprox.asMilliseconds();
         lifetime = sf::milliseconds((myrand() % lax) * 0.25 + lax);
     }
+
+    [[nodiscard]] unsigned moveMask() const {
+        switch(type) {
+            case T_SAND:
+            case T_SANDPINK:
+                return MOVES_D | MOVES_LRD | MOVES_FORCE;
+            case T_WATER:
+                return MOVES_D | MOVES_LRD | MOVES_LR;
+            case T_SMOKE:
+                return MOVES_U | MOVES_LRU | MOVES_LR;
+            default:
+                return MOVES_NONE;
+        }
+    }
 };
 
 class ParticleSystem : public sf::Drawable, public sf::Transformable {
     Particle *m_particles;
-    sf::VertexArray m_vertices;
     std::vector<std::pair<int, int>> m_changes;
-    int m_spawnType = SAND;
+    sf::VertexArray m_vertices;
+    types m_spawnT = T_SAND;
 
 public:
     explicit ParticleSystem() :
@@ -86,7 +109,7 @@ public:
                 auto &v = m_vertices[(x + W * y) + W*H*(px+py*PIXEL_SIZE)];
                 v.position.x = x * PIXEL_SIZE + px;
                 v.position.y = y * PIXEL_SIZE + py;
-                v.color = sf::Color(EMPTY_C);
+                v.color = sf::Color(T_EMPTY);
             }
     }
 
@@ -94,133 +117,72 @@ public:
         delete[] m_particles;
     }
 
-    void handleMouse(sf::Vector2f position, int wheelDelta) {
+    void handleMouse(const sf::Vector2f &position, const int wheelDelta) {
+        int newBrushSize = brushSize + wheelDelta * 4;
+        if (wheelDelta && newBrushSize > 0)
+            brushSize = newBrushSize;
+
+        if(!inBounds((int)position.x/PIXEL_SIZE, (int)position.y/PIXEL_SIZE)) return;
+
         if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
             removeParticles(position);
         else if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
             spawnParticles(position);
-
-        int newBrushSize = brushSize + wheelDelta * 4;
-        if (wheelDelta && newBrushSize > 0)
-            brushSize = newBrushSize;
     }
 
-    void handleKeyboard(sf::Event::KeyEvent keycode) {
-        if     (keycode.code == sf::Keyboard::Numpad0)
-            m_spawnType = SAND;
-        else if(keycode.code == sf::Keyboard::Numpad1)
-            m_spawnType = WATER;
-        else if(keycode.code == sf::Keyboard::Numpad2)
-            m_spawnType = STONE;
+    void handleKeyboard(const sf::Event::KeyEvent keycode) {
+        if     (keycode.code == sf::Keyboard::Numpad7)
+            m_spawnT = T_SAND;
+        else if(keycode.code == sf::Keyboard::Numpad8)
+            m_spawnT = T_WATER;
+        else if(keycode.code == sf::Keyboard::Numpad9)
+            m_spawnT = T_STONE;
+        else if(keycode.code == sf::Keyboard::Numpad4)
+            m_spawnT = T_SANDPINK;
     }
 
-    void update(sf::Time elapsed) {
-        clearMoved();
-
-        updateRange(elapsed, 0, H);
-
+    void update(const sf::Time &elapsed) {
+        for (int y = 0; y < H; ++y)
+            for (int x = 0; x < W; ++x)
+                updateParticle(elapsed, x, y);
         commitMoves();
-//        updateVertices();
-
-//        for (int i = m_h - 1; i >= 0; --i) {
-//        for(int j = 0; j < m_w; ++j){
-
-//        for(int i = 0; i < m_h; ++i){
-//            for (int j = m_w - 1; j >= 0; --j) {
-//                updateParticle(j + m_w * i, elapsed);
-//            }
-//        }
     }
 
-    void update_singlethread(sf::Time elapsed) {
-        clearMoved();
-
-        std::thread t1(
-                &ParticleSystem::updateRange, this, elapsed, 0, H
-        );
-        t1.join();
-
-        commitMoves();
-        updateVertices();
-
-    }
-
-    void update_multithread(sf::Time elapsed) {
-        clearMoved();
-
-        const int slice = H / ITHREADS;
-        if (slice < 10) { // irrevelant as for today
-            cout << "ERROR: too many threads, slice = " << slice;
-            exit(1);
-        }
-        std::vector<std::thread> threads;
-
-        for (int ijob = 0; ijob < ITHREADS; ijob += 2)
-            threads.push_back(std::thread(
-                    &ParticleSystem::updateRange, this, elapsed, ijob * slice, (ijob + 1) * slice
-            ));
-        for (auto &thread : threads)
-            thread.join();
-
-        threads.clear();
-        for (int ijob = 1; ijob < ITHREADS; ijob += 2)
-            threads.emplace_back(
-                    &ParticleSystem::updateRange, this, elapsed, ijob * slice, (ijob + 1) * slice
-            );
-        int leftovers = slice * ITHREADS;
-        if (leftovers)
-            threads.emplace_back(
-                    &ParticleSystem::updateRange, this, elapsed, leftovers, H
-            );
-        for (auto &thread : threads)
-            thread.join();
-
-
-        commitMoves();
-        updateVertices();
+    void fillHalf() { // debug
+        for(int y = 0; y < H/2; y++)
+            for(int x = 0; x < W; x++) {
+                m_particles[x + W*y].type = T_SAND;
+                updateVertex(x+W*y);
+            }
     }
 
 private:
-    void updateRange(const sf::Time elapsed, const int ym, const int yM) {
-        for (int y = ym; y < yM; ++y) {
-            for (int x = 0; x < W; ++x) {
-                updateParticle(elapsed, x, y);
-            }
-        }
-    }
-
-    void clearMoved() {
-        for (int i = 0; i < W * H; i++)
-            m_particles[i].updated = false;
-    }
-
-    void spawnParticles(sf::Vector2f &position) {
+    void spawnParticles(const sf::Vector2f &position) {
         Particle *p;
-        int fillPercentage = 1;
         for (int x = -brushSize; x <= brushSize; x++) {
-            int circle = sqrt(pow(brushSize, 2) - pow(x, 2));
+            const int circle = (int)sqrt(pow(brushSize, 2) - pow(x, 2));
             for (int y = -circle; y <= circle; y++) {
-                if (m_spawnType != STONE && myrand() % 100 > fillPercentage) continue;
+                if (m_spawnT != T_STONE
+                    && myrand() % 100 > FILL_PERC) continue;
                 const int nx = position.x / PIXEL_SIZE + x;
                 const int ny = position.y / PIXEL_SIZE + y;
                 if (xytop(nx, ny, p) && !p->type) {
-                    p->type = m_spawnType;
-//                    Particle p2;
+                    p->type = m_spawnT;
                     updateVertex(nx+W*ny);
                 }
             }
         }
     }
 
-    void removeParticles(sf::Vector2f &position) {
+    void removeParticles(const sf::Vector2f &position) {
         Particle *p;
         for (int x = -brushSize; x <= brushSize; x++) {
-            int circle = sqrt(pow(brushSize, 2) - pow(x, 2));
+            int circle = (int)sqrt(pow(brushSize, 2) - pow(x, 2));
             for (int y = -circle; y <= circle; y++) {
                 const int nx = position.x / PIXEL_SIZE + x;
                 const int ny = position.y / PIXEL_SIZE + y;
                 if (xytop(nx, ny, p) && p->type) {
-                    p->type = EMPTY;
+                    p->type = T_EMPTY;
                     updateVertex(nx+W*ny);
                 }
             }
@@ -241,9 +203,10 @@ private:
 //    }
 
     bool getNextPosition(int &x, int &y, Particle *&p, Particle *&np) const {
-        int swapWith = EMPTY; // (tnp->type & swapWith)
         Particle *tnp = nullptr;
         int ty, tx, im, iM;
+        auto mm = p->moveMask();
+
 //        if(p->velocity.x && p->velocity.y) {
 //            iM = p->velocity.y; // +1 +2 maybe even
 //            for (int i = 1; i <= iM; ++i) {
@@ -257,7 +220,7 @@ private:
 //            }
 //            if (np) return true;
 //        } else
-//        if (p->velocity.x) { // TODO check without else
+//        if (p->velocity.x) {
 //            im = x;
 //            iM = x + ((int)p->velocity.x & -1);
 //            if(im > iM)
@@ -270,147 +233,143 @@ private:
 //            }
 //            if (np) return true;
 //        } else
-        if (p->velocity.y) {
-            im = y;
-            iM = y + ((int)p->velocity.y & -1);
-            if(im > iM)
-                std::swap(im, iM);
-            for (int ty = im; ty <= iM; ++ty) {
-                if (xytop(x, ty, tnp) && tnp->type == swapWith) {
-                    np = tnp;
-                    y = ty;
-                } else break;
-            }
-            if (np) return true;
-        }
 
-        ty = y + 1;
-        if (xytop(x, ty, tnp) && tnp->type == swapWith) {
-            np = tnp;
-            y = ty;
-            return true;
+        auto canSwap = [mm, &p](Particle *& tnp) {
+            return (tnp->type == T_EMPTY
+                    || ((mm & MOVES_FORCE)
+                        && tnp->type != T_STONE
+                        && tnp->type != p->type));
+        };
+
+//        if (p->velocity.y) { // TODO change to Bresenham algorithm
+//            im = y;
+//            iM = y + (p->velocity.y & -1);
+//            if(im > iM)
+//                std::swap(im, iM);
+//            for (ty = im; ty <= iM; ++ty) {
+//                if (xytop(x, ty, tnp) && canSwap(tnp)) {
+//                    np = tnp;
+//                    y = ty;
+//                } else break;
+//            }
+//            if (np) return true;
+//        }
+
+        if(mm & MOVES_D) {
+            ty = y + 1;
+            if (xytop(x, ty, tnp) && canSwap(tnp)) {
+                np = tnp;
+                y = ty;
+                return true;
+            }
         }
-        if (p->type == WATER) {
+        if(mm & MOVES_LR) {
             int direction = p->velocity.x
-                            ? ((int)p->velocity.x & -1)
+                            ? (p->velocity.x & -1)
                             : (myrand() % 2) ? 1 : -1;
             tx = x + direction;
-            if (xytop(tx, y, tnp) && tnp->type == swapWith) {
+            if (xytop(tx, y, tnp) && canSwap(tnp)) {
                 np = tnp;
                 x = tx;
                 return true;
             }
             tx = x - direction;
-            if (xytop(tx, y, tnp) && tnp->type == swapWith) {
+            if (xytop(tx, y, tnp) && canSwap(tnp)) {
                 np = tnp;
                 x = tx;
                 return true;
             }
         }
-        int direction = p->velocity.x
-                ? ((int)p->velocity.x & -1)
-                : (myrand() % 2) ? 1 : -1;
-        tx = x + direction;
-        if (xytop(tx, ty, tnp) && tnp->type == swapWith) {
-            np = tnp;
-            x = tx;
-            y = ty;
-            return true;
-        }
-        tx = x - direction;
-        if (xytop(tx, ty, tnp) && tnp->type == swapWith) {
-            np = tnp;
-            x = tx;
-            y = ty;
-            return true;
+        if(mm & MOVES_LRD) {
+            int direction = p->velocity.x
+                            ? (p->velocity.x & -1)
+                            : (myrand() % 2) ? 1 : -1;
+            tx = x + direction;
+            if (xytop(tx, ty, tnp) && canSwap(tnp)) {
+                np = tnp;
+                x = tx;
+                y = ty;
+                return true;
+            }
+            tx = x - direction;
+            if (xytop(tx, ty, tnp) && canSwap(tnp)) {
+                np = tnp;
+                x = tx;
+                y = ty;
+                return true;
+            }
         }
         return false;
     }
 
-    void updateParticle(sf::Time elapsed, int x, int y) {
-        auto idx = x + W * y;
+    void updateParticle(const sf::Time &elapsed, int x, int y) {
+        auto idx = x+W*y;
         auto *p = &m_particles[idx];
-        if (!p->type || p->type == STONE) return;
-        if (p->updated) return;
+        if (p->type == T_EMPTY || p->type == T_STONE) return;
 
 //        if (!updateParticleTime(idx, p, elapsed)) return;
 
         auto nx = x, ny = y, nidx = idx;
         Particle *np = nullptr;
         if (getNextPosition(nx, ny, p, np)) {
-            nidx = nx + W * ny;
 //            p->velocity.x = (nx - x);
             p->velocity.x = (nx - x) & -1;
-            p->velocity.y = pow(ny - y + 1, 2);
+            p->velocity.y = (int)pow(ny - y + 1, 2);
+            nidx = nx+W*ny;
             m_changes.emplace_back(nidx, idx);
-        }
-        else {
-            p->velocity.y = 0;
+        } else {
             p->velocity.x = 0;
+            p->velocity.y = 0;
         }
     }
 
     void commitMoves() {
-        std::sort(m_changes.begin(), m_changes.end(),
+        std::sort(std::execution::par_unseq, m_changes.begin(), m_changes.end(),
         [](auto& a, auto& b) { return a.first < b.first; }
         );
+
+//        __gnu_parallel::sort(m_changes.begin(), m_changes.end(),
+//        [](auto& a, auto& b) { return a.first < b.first; }
+//        );
+
         m_changes.emplace_back(-1, -1); // catch final move
 
         int iprev = 0;
-        for (size_t i = 0; i < m_changes.size() - 1; i++) {
-            // clear moved
+        for (int i = 0; i < m_changes.size() - 1; i++) {
             if (m_changes[i + 1].first != m_changes[i].first) {
-                // update vertex
                 int rand = iprev + myrand() % (i - iprev + 1);
                 int dst = m_changes[rand].first;
                 int src = m_changes[rand].second;
-                std::swap(m_particles[src], m_particles[dst]);
-                iprev = i + 1;
-
+                std::swap(m_particles[dst], m_particles[src]);
                 updateVertex(dst);
                 updateVertex(src);
+                iprev = i + 1;
             }
         }
         m_changes.clear();
     }
 
-    void updateVertices() {
-        for (int i = 0; i < W * H; i++) {
-            updateVertex(i);
-        }
-    }
-
-    void updateVertex(int index) {
+    inline void updateVertex(const int index) {
         for(int pixel = 0; pixel < PIXEL_SIZE*PIXEL_SIZE; pixel++) {
             auto &p = m_particles[index];
-            auto &v = m_vertices[index + W * H * pixel];
-            switch (p.type) {
-                case SAND:
-                    v.color = sf::Color(SAND_C);
-                    continue;
-                case WATER:
-                    v.color = sf::Color(WATER_C);
-                    continue;
-                case STONE:
-                    v.color = sf::Color(STONE_C);
-                    continue;
-                default:
-                    v.color = sf::Color(EMPTY_C);
-                    continue;
-            }
+            auto &v = m_vertices[index + W*H*pixel];
+            v.color = sf::Color(p.type);
         }
     }
 
-    virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override {
 //        states.transform *= getTransform();
         states.texture = nullptr;
         target.draw(m_vertices, states);
     }
 
-    inline bool xytop(int x, int y, Particle *&p) const {
-        if (x < 0 || x >= W || y < 0 || y >= H)
-            return false;
-        p = &m_particles[x + W * y];
+    static inline bool inBounds(const int x, const int y) {
+        return x >= 0 && x < W && y >= 0 && y < H;
+    }
+
+    inline bool xytop(const int x, const int y, Particle *&p) const {
+        if (!inBounds(x, y)) return false;
+        p = &m_particles[x+W*y];
         return true;
     }
 };
@@ -423,10 +382,10 @@ int main() {
     sf::Clock clock;
 
     ParticleSystem particles;
+//    particles.fillHalf();
 
     // run the main loop
-    long lc = 0;
-    long ms = 0;
+    long lc = 0, ms = 0;
     while (window.isOpen()) {
         // handle events
         sf::Event event{};
@@ -439,21 +398,18 @@ int main() {
             else if (event.type == sf::Event::KeyPressed)
                 particles.handleKeyboard(event.key);
 
-        sf::Vector2i mouse = sf::Mouse::getPosition(window);
+        auto mouse = sf::Mouse::getPosition(window);
         particles.handleMouse(window.mapPixelToCoords(mouse), wheelDelta);
 
-        sf::Time elapsed = clock.restart();
+        auto elapsed = clock.restart();
         particles.update(elapsed);
-//        particles.update_singlethread(elapsed);
-//        particles.update_multithread(elapsed);
 
         ms += elapsed.asMicroseconds();
         lc++;
         if (ms >= 1000000) { // show fps every second
             int fps = 1000000 / ((float) ms / lc);
             cout << fps << " fps" << endl;
-            ms = 0;
-            lc = 0;
+            ms = lc = 0;
         }
 
         window.clear();
